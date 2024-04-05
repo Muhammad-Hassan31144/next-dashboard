0762f911069286f4bc6a3139bbaae6afdd4f1a55
Muhammad-Hassan31144 <31144@students.riphah.edu.pk>
2024-04-06 01:01:21 +0500

    Fixed Routes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana, Roboto_Condensed } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const robotoCondensed = Roboto_Condensed({ weight: ['400', '700'],
+    subsets: ['latin'] }); 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
